c2aa141078803ffd6f6f2fbc01717e74aabf6c62
khussalpradhan <pradhankhu@gmail.com>
2025-11-16 01:08:58 -0600

    fixed for gloabl ssl verification issue in action mailer

--- config/initializers/disable_global_ssl_verification.rb
+++ config/initializers/disable_global_ssl_verification.rb
@@ -1,10 +1,14 @@
 Rails.application.config.after_initialize do
   if Rails.env.development?
     begin
-      if defined?(ActionMailer::Base)
-        ActionMailer::Base.smtp_settings ||= {}
-        ActionMailer::Base.smtp_settings[:openssl_verify_mode] = "none"
+      config_am = Rails.application.config.action_mailer
+      if config_am.nil?
+        Rails.application.config.action_mailer = ActiveSupport::OrderedOptions.new
+        config_am = Rails.application.config.action_mailer
       end
+
+      config_am.smtp_settings ||= {}
+      config_am.smtp_settings[:openssl_verify_mode] = "none"
     rescue => e
       warn "disable_global_ssl_verification: failed to set ActionMailer smtp_settings: #{e.class}: #{e.message}"
     end
