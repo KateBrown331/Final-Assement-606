3baac2dc158805136953579cc3c885a66d966450
Aurora <aurawra@outlook.com>
2025-11-18 22:47:42 -0600

    Added Bootstrap + Login & Signup Styling
    Added bootstrap page and login/signup styling for more aesthetically pleasing and cohesive application

--- Gemfile
+++ Gemfile
@@ -36,9 +36,8 @@ gem "kamal", require: false
 gem "thruster", require: false
 
 ### Asset Pipeline Stuff ###
-# Tailwind for Rails
-gem "tailwindcss-rails", "~> 4.4"
-# Switch rails back to sprockets to prevent having to load asset pipeline
+gem "bootstrap", "~> 5.3"
+gem "sassc-rails"
 gem "sprockets-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,19 +6,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # Look up user by email
-    user = User.find_by(email: params[:user][:email])
+    user = User.find_by(email: params[:email])
 
-    if user && user.authenticate(params[:user][:password])
-      # Login success
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to profile_path, notice: "Logged in successfully!"
     else
-      # Login failed
-      flash.now[:error] = "Invalid email or password"
+      flash.now[:alert] = "Invalid email or password"
       render :new
     end
   end
+
 
   def destroy
     session[:user_id] = nil
