9a8d78f2cd7c4bf2e92f9aca5290c6e2de2950f4
David Liu <david_liu@tamu.edu>
2025-11-19 21:50:22 -0600

    Fixed verification call in CompanyVerificationController
    This PR addresses a previous issue with verifying company email.

--- app/mailers/company_mailer.rb
+++ app/mailers/company_mailer.rb
@@ -3,7 +3,7 @@ class CompanyMailer < ApplicationMailer
     @current_user = verification_entry.user
     @company_email = verification_entry.company_email
     @company_name  = verification_entry.company_name
-    @verification_link = company_verifications_valify_path(id: verification_entry.id, token: verification_entry.verification_token)
+    @verification_link = company_verifications_verify_path(id: verification_entry.id, token: verification_entry.verification_token)
     puts "DEBUG: Verification link - #{@verification_link}"
     mail(to: verification_entry.company_email, subject: "Verify Your Email for #{verification_entry.company_name}!")
   end
